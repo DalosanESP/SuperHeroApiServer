90492c5c8170c6fc727885b3a968dac9aa2794ce
DalosanESP <dalosanesp@gmail.com>
2023-09-18 14:23:16 +0200

    Promise DB

--- config/db.js
+++ config/db.js
@@ -9,10 +9,13 @@
 //     database: process.env.DB_DATABASE,
 //   });
 
-import pkg from 'pg';
-const { Pool } = pkg;
+import pgPromise from 'pg-promise';
 
-const pool = new Pool({
+// Inicializa pg-promise
+const pgp = pgPromise();
+
+// Configura la conexión a la base de datos
+const db = pgp({
   user: 'fl0user',
   host: 'ep-still-dew-39348852.eu-central-1.aws.neon.tech',
   database: 'superheroApiUsers',
@@ -25,13 +28,18 @@ const pool = new Pool({
 
 // Función para ejecutar una consulta SELECT
 export async function runSelectQuery(query) {
-  const client = await pool.connect();
   try {
     console.log(query);
-    const result = await client.query(query);
-    return result.rows;
+
+    // Utiliza db.any para ejecutar consultas
+    const result = await db.any(query);
+
+    return result;
   } catch (err) {
     console.error('Error al ejecutar la consulta:', err);
-    throw err; // Puedes manejar el error según tus necesidades
+
+    // Manejo de errores, según sea necesario
+
+    throw err;
   }
 }
